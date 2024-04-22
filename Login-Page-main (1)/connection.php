d21a71da4f3035270033418803e574c92a69e2c8
A.S.Vignesh Raja <136581635+A-S-Vignesh-Raja@users.noreply.github.com>
2024-04-22 17:27:58 +0530

    Update connection.php

--- Login-Page-main (1)/connection.php
+++ Login-Page-main (1)/connection.php
@@ -1,7 +1,7 @@
 <?php
     $servername='localhost';
     $username='root';
-    $password='viki2003';
+    $password='';
     $dbname = "dashboard";
     $con=mysqli_connect($servername,$username,$password,$dbname);
     if(!$con){
